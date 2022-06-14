983ede3ff8cf9629f1222e826130b295efbbc829
jaewon2336 <xldzjqpf1588@naver.com>
2022-06-14 14:37:54 +0900

    Initail commit

---
+++ src/main/java/site/metacoding/baseballmanage/BaseballManageApplication.java
@@ -0,0 +1,13 @@
+package site.metacoding.baseballmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseballManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseballManageApplication.class, args);
+	}
+
+}
